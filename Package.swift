fda841ed2c3e4fdb14238dee9115f7e8a986618d
Ivan Valkou <zloyfey@gmail.com>
2021-03-17 14:03:01 +0300

    fix archive

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
                     name: "TrackingKit",
-                    url: "https://dl.dropbox.com/s/nh86afx59a534px/TrackingKit.1.0.0.zip",
-                    checksum: "d71c12ab3bb0727183adb4a1f6f1ef54b729668451ce915d5e0fb232617895ef"
+                    url: "https://dl.dropbox.com/s/4qt64429to1rsjs/TrackingKit.framework.zip",
+                    checksum: "bd43dba1cc093957a0059989d862fc550a5aca840d3666e19d45f91f185add36"
                 ),
     ]
 )
