2afec5e83356811c7aa7fa82d10b172d538722c4
Teamcity <teamcity@dev-autotest-b-16d2.local>
2022-11-30 14:52:56 +0300

    Release 1.0.56

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TrackingKit",
-            url: "https://mmr.jfrog.io/artifactory/tracking-sdk-ios/1.0.55/TrackingKit.xcframework.zip",
-            checksum: "6024e3dae43dd3050733697b2604dc70fe7b09686288ff360109106ca3438698"
+            url: "https://mmr.jfrog.io/artifactory/tracking-sdk-ios/1.0.56/TrackingKit.xcframework.zip",
+            checksum: "44899fe59b94de4a398546ef05f4b78a9da3527e8831a782cb3be4874d5b7790"
         )
     ]
 )
