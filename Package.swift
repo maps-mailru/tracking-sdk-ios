4e83cdc4fd6dbff4e9770d58c0a83335e47c297e
mapsagent <mapsagent@vk.team>
2023-06-16 12:55:45 +0300

    Release 1.0.59

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TrackingKit",
-            url: "https://mmr.jfrog.io/artifactory/tracking-sdk-ios/1.0.57/TrackingKit.xcframework.zip",
-            checksum: "e6608705f9c185bcf6532392beeefec04e1bf99f6966f9b102f2c22b03670c06"
+            url: "https://artifactory-external.vkpartner.ru/artifactory/tracking-sdk-ios/1.0.59/TrackingKit.xcframework.zip",
+            checksum: "31fe33421a2ec8beb08160e28bc4c94df7d554d1ed372dd4cdd3364ddd037611"
         )
     ]
 )
